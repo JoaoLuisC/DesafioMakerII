1d3bebd807feaea844b6ca11f194b7ee378f75a7
Joao Luis Cardoso <joao.lc12@hotmail.com>
2024-03-13 16:56:17 -0300

    criaçaõ da pagina itens e seus css e js respectivos

--- script/sheetsApi.js
+++ script/sheetsApi.js
@@ -3,12 +3,10 @@ function loadGoogleSheetData() {
     const spreadsheetId = '1MsFl6hMKJusPb8m9rXBzmZ5iRVkLNmKg3JsOQPh4Kw4';
     // ID da planilha dentro do documento (geralmente 0 para a primeira planilha)
     const sheetId = 0;
-    // Linha inicial para começar a leitura (exemplo: começando da linha 2)
-    const startRow = 2;
 
     gapi.client.sheets.spreadsheets.values.get({
         spreadsheetId: spreadsheetId,
-        range: 'ComponentesEletronicos!A' + startRow + ':B' // Substitua 'ComponentesEletronicos' pelo nome da aba e ajuste o intervalo conforme necessário
+        range: 'ComponentesEletronicos' // Substitua 'ComponentesEletronicos' pelo nome da aba e ajuste o intervalo conforme necessário
     }).then(function (response) {
         const data = response.result.values;
         const tableBody = document.querySelector('#actions-table tbody');
@@ -18,11 +16,9 @@ function loadGoogleSheetData() {
 
         // Preencha a tabela com os dados da planilha
         data.forEach(function (row) {
-            // Extrai apenas os dados das colunas A e B
             const descricao = row[0] || ''; // Coluna A
             const quantidade = row[1] || ''; // Coluna B
 
-            // Cria uma nova linha na tabela e adiciona os dados das colunas A e B
             const tableRow = document.createElement('tr');
             const cellDescricao = document.createElement('td');
             const cellQuantidade = document.createElement('td');
@@ -51,5 +47,3 @@ function initGoogleSheetsApi() {
 
 // Carrega a API do Google Sheets e inicia a aplicação
 gapi.load('client', initGoogleSheetsApi);
-
-
