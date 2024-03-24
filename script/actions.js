08eafedc15e69eacac7e8df90882954434997c04
unknown <salomao.junior2k22@gmail.com>
2024-03-24 12:05:27 -0300

    att index sections, img, etc

--- script/actions.js
+++ script/actions.js
@@ -2,7 +2,7 @@ window.addEventListener("scroll", function () {
     let header = document.querySelector('.navbar');
     let headerHidden = document.querySelector('.navbar-hidden');
     headerHidden.classList.toggle('header-scroll-visible', window.scrollY > 0)
-    header.classList.toggle('header-scroll', window.scrollY > 1)
-    
+    header.classList.toggle('header-scroll', window.scrollY > 0)
+
 
 })
